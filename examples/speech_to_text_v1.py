0e19df3facee37775735f2de7b2a0cfc7a20aa8c
BI876578 <BI876578@jp.ibm.com>
2018-05-17 13:47:29 +0900

    -

--- examples/speech_to_text_v1.py
+++ examples/speech_to_text_v1.py
@@ -5,10 +5,12 @@ from watson_developer_cloud import SpeechToTextV1
 from watson_developer_cloud.websocket import RecognizeCallback
 
 speech_to_text = SpeechToTextV1(
-    username='YOUR SERVICE USERNAME',
-    password='YOUR SERVICE PASSWORD',
+    username="76a7855d-3461-4c08-8b1e-08e4865771b7",
+    password="H2yFt16tqSBT",
     url='https://stream.watsonplatform.net/speech-to-text/api')
 
+
+'''
 print(json.dumps(speech_to_text.list_models(), indent=2))
 
 print(json.dumps(speech_to_text.get_model('en-US_BroadbandModel'), indent=2))
@@ -23,17 +25,20 @@ with open(join(dirname(__file__), '../resources/speech.wav'),
                 timestamps=True,
                 word_confidence=True),
             indent=2))
+'''
 
 # Example using websockets
 class MyRecognizeCallback(RecognizeCallback):
     def __init__(self):
         RecognizeCallback.__init__(self)
 
-    def on_transcription(self, transcript):
+    '''
+	def on_transcription(self, transcript):
         print(transcript)
 
     def on_connected(self):
         print('Connection was successful')
+    '''
 
     def on_error(self, error):
         print('Error received: {}'.format(error))
@@ -44,8 +49,10 @@ class MyRecognizeCallback(RecognizeCallback):
     def on_listening(self):
         print('Service is listening')
 
-    def on_transcription_complete(self):
+    '''
+	def on_transcription_complete(self):
         print('Transcription completed')
+    '''
 
     def on_hypothesis(self, hypothesis):
         print(hypothesis)
